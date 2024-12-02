2064c969cafc4fa1dbe0dbcd64a0c46511445f58
Rietty <rie.juliette@gmail.com>
2024-12-02 00:07:15 -0500

    Finish 2024, Day 2.

--- src/solutions/2024/day02.py
+++ src/solutions/2024/day02.py
@@ -1,4 +1,4 @@
-from typing import TypeVar
+from typing import List, TypeVar
 
 from utils.decorators import benchmark
 from utils.solver import Problem, solve_problem
@@ -7,18 +7,44 @@ from utils.solver import Problem, solve_problem
 ParsedType = TypeVar("ParsedType")
 
 
+def is_safe(row: List[int]) -> bool:
+    # Check to see if it is strictly decreasing or strictly increasing
+    if sorted(row) == row or sorted(row, reverse=True) == row:
+        # Check to see the difference between any 2 adjacent numbers is at most 3 and at least 1
+        if all(abs(a - b) <= 3 for a, b in zip(row, row[1:])) and all(
+            abs(a - b) >= 1 for a, b in zip(row, row[1:])
+        ):
+            return True
+    return False
+
+
 @benchmark
 def part_a(data: ParsedType) -> int:
-    return 0
+    return sum(is_safe(row) for row in data)
 
 
 @benchmark
 def part_b(data: ParsedType) -> int:
-    return 0
+    # Do same thing as part a, but also check if removing
+    # For each row of data, check if it is safe or not.
+    # If it is not safe, then check if removing any 1 element makes it safe
+    # If it does, then increment the count
+    safe = 0
+    for row in data:
+        if is_safe(row):
+            safe += 1
+        else:
+            for i in range(len(row)):
+                if is_safe(row[:i] + row[i + 1 :]):
+                    safe += 1
+                    break
+
+    return safe
 
 
 def parse(data: str) -> ParsedType:
-    return data  # Replace with actual parsing logic.
+    # Return a list of list of numbers
+    return [[int(x) for x in line.split()] for line in data.strip().splitlines()]
 
 
 def solve(problem: Problem) -> None:
@@ -32,8 +58,18 @@ def solve(problem: Problem) -> None:
     )
 
 
-test_data_a = """
+test_data_a = """7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
 """
 
-test_data_b = """
+test_data_b = """7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
 """
