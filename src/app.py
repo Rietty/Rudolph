e7fcbb90acf735e7c9fb2d1a2bcda35b1ce4d9ec
Rietty <rie.juliette@gmail.com>
2024-12-03 16:00:27 -0500

    Finish 2021, Day 3 - Part A. Refactor some other stuff, add logging. Update logging.

--- src/app.py
+++ src/app.py
@@ -1,38 +1,46 @@
 import importlib
 import logging
+import logging.config
+import os
 
-import click
+import cloup
+import yaml
 
 from utils.solver import solve_problem, test_problem
 
 log = logging.getLogger(__name__)
 
 
-@click.command()
-@click.option(
-    "--problem",
-    "-p",
-    type=str,
-    required=True,
-    help="The problem to solve in the format 'year/day/part', e.g., '2024/1/a'.",
+@cloup.command()
+@cloup.option("--year", "-y", type=int, required=True, help="Problem year.")
+@cloup.option("--day", "-d", type=int, required=True, help="Problem day.")
+@cloup.option("--part", "-p", type=str, required=True, help="Problem part.")
+@cloup.option_group(
+    "Data",
+    "Deciding on data source and destination.",
+    cloup.option(
+        "--test",
+        "-t",
+        is_flag=True,
+        default=False,
+        help="Solve the test input instead of the puzzle input.",
+    ),
+    cloup.option(
+        "--submit",
+        "-s",
+        is_flag=True,
+        default=False,
+        help="Submit the solution after solving it.",
+    ),
+    cloup.option("--file", "-f", type=str, default=None, help="Path to an input file."),
+    constraint=cloup.constraints.mutually_exclusive,
 )
-@click.option(
-    "--test",
-    "-t",
-    is_flag=True,
-    help="Solve the test input instead of the puzzle input.",
-)
-@click.option(
-    "--submit", "-s", is_flag=True, help="Submit the solution after solving it."
-)
-def main(problem, test, submit):
+def main(**kwargs):
     """Solve a specific Advent of Code problem."""
     try:
-        # Parse the problem argument
-        year, day, part = problem.split("/")
-        year = int(year)
-        day = int(day)
-        part = part.lower()  # Normalize part to lowercase (e.g., 'a', 'b')
+        year = kwargs["year"]
+        day = kwargs["day"]
+        part = kwargs["part"].lower()
 
         if part not in ["a", "b"]:
             raise ValueError("Part must be 'a' or 'b'.")
@@ -43,10 +51,8 @@ def main(problem, test, submit):
         # Check if the module exists
         module = importlib.import_module(module_name)
 
-        if test:
-            print(
-                f"Testing {year}/{day_formatted.capitalize()} - Part {part.upper()}..."
-            )
+        if kwargs["test"] or kwargs["file"]:
+            log.info(f"Testing Day {day}, {year}, Part {part.upper()}...")
             test_problem(
                 part=part,
                 parse=module.parse,
@@ -54,28 +60,41 @@ def main(problem, test, submit):
                 part_b=module.part_b,
                 test_data_a=module.test_data_a,
                 test_data_b=module.test_data_b,
+                file=kwargs["file"],
             )
         else:
-            print(
-                f"Solving {year}/{day_formatted.capitalize()} - Part {part.upper()}..."
-            )
+            log.info(f"Solving Day {day}, {year}, Part {part.upper()}...")
             solve_problem(
                 year=year,
                 day=day,
                 part=part,
-                publish=submit,
+                publish=kwargs["submit"],
                 parse=module.parse,
                 part_a=module.part_a,
                 part_b=module.part_b,
             )
 
     except ValueError as e:
-        print(f"Invalid format: {e}. Use 'year/day/part', e.g., '2024/1/a'.")
+        log.error(f"{e}.")
     except ModuleNotFoundError:
-        print(f"Module for problem '{problem}' not found.")
+        log.error(f"Module '{year}/{day}/{part}' not found.")
     except Exception as e:
-        print(f"An error occurred: {e}")
+        log.error(f"{e}")
 
 
 if __name__ == "__main__":
+    # Load logging configuration.
+    log_config_path = os.path.join(os.path.dirname(__file__), "config", "logger.yml")
+
+    with open(log_config_path, "r") as f:
+        log_config = yaml.safe_load(f.read())
+
+    # Set up logging suppression of external loggers.
+    logging.getLogger("aocd").setLevel(logging.ERROR)
+    logging.getLogger("urllib3").setLevel(logging.ERROR)
+
+    # Assign configuration to the logger.
+    logging.config.dictConfig(log_config)
+
+    # Start our application.
     main()
