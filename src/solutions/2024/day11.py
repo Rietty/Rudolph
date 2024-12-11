c9274344aa0acf1e285feee5f5f752cea366168d
Juliette H. Lavoie <35241556+Rietty@users.noreply.github.com>
2024-12-11 11:27:12 -0500

    Finished 2024, Day 11.

---
+++ src/solutions/2024/day11.py
@@ -0,0 +1,58 @@
+import logging
+import math
+from collections import defaultdict
+from functools import lru_cache, reduce
+
+from utils.decorators import benchmark
+
+log = logging.getLogger(__name__)
+
+
+@lru_cache
+def count_digits(n: int) -> int:
+    return math.floor(math.log(n, 10) + 1)
+
+
+@lru_cache
+def split_number(n: int, d: int) -> int:
+    div = 10 ** (d // 2)
+    right = n % div
+    left = n // div
+    return left, right
+
+
+def apply_rules[T](data: T) -> T:
+    new_stones = defaultdict(lambda: 0)
+
+    for s, f in data.items():
+        if s == 0:
+            new_stones[1] += f
+        elif (d := count_digits(s)) % 2 == 0:
+            l, r = split_number(s, d)
+            new_stones[l] += f
+            new_stones[r] += f
+        else:
+            new_stones[s * 2024] += f
+
+    return new_stones
+
+
+@benchmark
+def part_a[T](data: T) -> int:
+    data = reduce(lambda data, _: apply_rules(data), range(25), data)
+    return sum(data.values())
+
+
+@benchmark
+def part_b[T](data: T) -> int:
+    data = reduce(lambda data, _: apply_rules(data), range(75), data)
+    return sum(data.values())
+
+
+def parse[T](data: str) -> T:
+    return {int(item): 1 for item in data.split()}
+
+
+test_data_a = """125 17"""
+
+test_data_b = test_data_a
