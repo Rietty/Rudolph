191570b641634b9f59fd0d9895d181562948000b
Rietty <rie.juliette@gmail.com>
2024-12-20 19:14:08 -0500

    Start work on a useful utility library.

---
+++ src/library/constants.py
@@ -0,0 +1,18 @@
+# Constants that will be commonly used when solving problems.
+from enum import Enum
+from typing import Final
+
+# The cardinal and ordinal directions
+Cardinals: Final = [
+    (-1, 0),  # North
+    (0, 1),  # East
+    (1, 0),  # South
+    (0, -1),  # West
+]
+
+Ordinals: Final = [
+    (-1, 1),  # North East
+    (1, 1),  # South East
+    (1, -1),  # South West
+    (-1, -1),  # North West
+]
