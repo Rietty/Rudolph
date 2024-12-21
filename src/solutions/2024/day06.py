81f25bb2fd0c07530c961cb64e63a0b3e88ced26
Rietty <rie.juliette@gmail.com>
2024-12-20 23:27:09 -0500

    Update day 6

--- src/solutions/2024/day06.py
+++ src/solutions/2024/day06.py
@@ -1,50 +1,44 @@
 from loguru import logger as log
 
+from library.constants import Cardinals
+from library.grid import Grid
 from utils.decorators import benchmark
-
-DIRECTIONS = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-
-
-def find_start[T](data: T) -> tuple[int, int]:
-    for x, row in enumerate(data):
-        if "^" in row:
-            return x, row.index("^")
-    return -1, -1
 
 
 def traverse_route[
     T
-](data: T, ex: int = -1, ey: int = -1, detect_loops: bool = False) -> tuple[set, bool]:
+](data: T, er: int = -1, ec: int = -1, detect_loops: bool = False) -> tuple[set, bool]:
     path = set()
-    n, m = len(data), len(data[0])
-    x, y = find_start(data)
-    if x == -1 or y == -1:
+    n, m = data.width, data.height
+    r, c = data.find_value("^")
+
+    if r == -1 or c == -1:
         return path, False
 
     dir = 0
     loop_detected = False
 
-    while 0 <= x < n and 0 <= y < m:
+    while 0 <= r < n and 0 <= c < m:
         old_dir = dir
-        dx, dy = DIRECTIONS[dir]
-        nx, ny = x + dx, y + dy
+        dr, dc = Cardinals[dir]
+        nr, nc = r + dr, c + dc
 
         while (
-            0 <= nx < n
-            and 0 <= ny < m
-            and (data[nx][ny] == "#" or (nx == ex and ny == ey))
+            0 <= nr < n
+            and 0 <= nc < m
+            and (data[nr][nc] == "#" or (nr == er and nc == ec))
         ):
             dir = (dir + 1) % 4
-            dx, dy = DIRECTIONS[dir]
-            nx, ny = x + dx, y + dy
+            dr, dc = Cardinals[dir]
+            nr, nc = r + dr, c + dc
 
-        path.add(((x, y), old_dir))
+        path.add(((r, c), old_dir))
 
-        if detect_loops and ((nx, ny), dir) in path:
+        if detect_loops and ((nr, nc), dir) in path:
             loop_detected = True
             break
 
-        x, y = nx, ny
+        r, c = nr, nc
 
     return path, loop_detected
 
@@ -61,20 +55,20 @@ def part_b[T](data: T) -> int:
     positions = {pos for pos, _ in visited}
     ans = 0
 
-    for x, y in positions:
-        if data[x][y] == ".":
-            data[x][y] = "#"
-            _, loop = traverse_route(data, x, y, detect_loops=True)
+    for r, c in positions:
+        if data[r][c] == ".":
+            data[r][c] = "#"
+            _, loop = traverse_route(data, r, c, detect_loops=True)
             if loop:
                 ans += 1
-            data[x][y] = "."
+            data[r][c] = "."
 
     return ans
 
 
 @benchmark
 def parse[T](data: str) -> T:
-    return [list(line) for line in data.splitlines()]
+    return Grid([list(line) for line in data.splitlines()])
 
 
 test_data_a = """....#.....
