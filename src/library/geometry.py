b8ba347f10136701ac316c37c9a1ccdf006f935c
Rietty <rie.juliette@gmail.com>
2024-12-20 19:14:08 -0500

    Start work on a useful utility library.

---
+++ src/library/geometry.py
@@ -0,0 +1,62 @@
+# Common geometry functions and calculations for various problems.
+from dataclasses import dataclass
+
+
+@dataclass
+class Point:
+    x: int
+    y: int
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        return Point(self.x * other.x, self.y * other.y)
+
+    def __truediv__(self, other):
+        return Point(self.x / other.x, self.y / other.y)
+
+    def __floordiv__(self, other):
+        return Point(self.x // other.x, self.y // other.y)
+
+    def __mod__(self, other):
+        return Point(self.x % other.x, self.y % other.y)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __ne__(self, other):
+        return self.x != other.x or self.y != other.y
+
+    def __lt__(self, other):
+        return self.x < other.x and self.y < other.y
+
+    def __le__(self, other):
+        return self.x <= other.x and self.y <= other.y
+
+    def __gt__(self, other):
+        return self.x > other.x and self.y > other.y
+
+    def __ge__(self, other):
+        return self.x >= other.x and self.y >= other.y
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+    def __repr__(self):
+        return f"Point({self.x}, {self.y})"
+
+
+def manhattan_distance(a: Point, b: Point) -> int:
+    return abs(a.x - b.x) + abs(a.y - b.y)
+
+
+def euclidean_distance(a: Point, b: Point) -> float:
+    return ((a.x - b.x) ** 2 + (a.y - b.y) ** 2) ** 0.5
+
+
+def chebyshev_distance(a: Point, b: Point) -> int:
+    return max(abs(a.x - b.x), abs(a.y - b.y))
---
+++ src/library/grid.py
@@ -0,0 +1,139 @@
+# Grid implementation that can be useful for various problems.
+from library.constants import Cardinals, Ordinals
+
+
+class Grid[T]:
+    """Grid class to represent a two-dimensional grid of values."""
+
+    def __init__(self, grid: list[list[T]]) -> None:
+        """Create a grid object from a list of lists.
+
+        Args:
+            grid (list[list[T]]): List of lists to create the grid from.
+        """
+        self.grid = grid
+        self.width = len(grid)
+        self.height = len(grid[0])
+
+    def get_neighbours(
+        self, r: int, c: int, diagonals: bool = False
+    ) -> list[tuple[int, int]]:
+        """Obtains the neighbours of a cell at a given row, col in the grid. Always from clockwise starting from the top.
+
+        Args:
+            r (int): Row of the cell.
+            c (int): Column of the cell.
+            diagonals (bool, optional): If we should include diagonals of the cell. Defaults to False.
+
+        Returns:
+            list[tuple[int, int]]: List of coordinates of the neighbours of the cell.
+        """
+        neighbours = []
+
+        for dr, dc in Cardinals:
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < self.width and 0 <= nc < self.height:
+                neighbours.append((nr, nc))
+
+        if diagonals:
+            for dr, dc in Ordinals:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < self.width and 0 <= nc < self.height:
+                    neighbours.append((nr, nc))
+
+        return neighbours
+
+    def get_neighbour_values(self, r: int, c: int, diagonals: bool = False) -> list[T]:
+        """Get the values of the neighbours of a cell at a given row, col in the grid.  Always from clockwise starting from the top.
+
+        Args:
+            r (int): Row of the cell.
+            c (int): Column of the cell.
+            diagonals (bool, optional): If we should include diagonals of the cell. Defaults to False.
+
+        Returns:
+            list[T]: List of the values of the neighbours of the cell.
+        """
+        return [self.grid[nr][nc] for nr, nc in self.get_neighbours(r, c, diagonals)]
+
+    def get_neighbour_rays(
+        self, r: int, c: int, scaling: int, diagonals: bool = False
+    ) -> list[list[T]]:
+        """Get the values of the neighbours of a cell at a given row, col in the grid.  Always from clockwise starting from the top.
+
+        Args:
+            r (int): Row of the cell.
+            c (int): Column of the cell.
+            scaling (int): How far to go in each direction.
+            diagonals (bool, optional): If we should include diagonals of the cell. Defaults to False.
+
+        Returns:
+            list[list[T]]: List of the values of the neighbours of the cell.
+        """
+        rays = []
+
+        for dr, dc in Cardinals:
+            ray = []
+            for i in range(1, scaling + 1):
+                nr, nc = r + dr * i, c + dc * i
+                if 0 <= nr < self.width and 0 <= nc < self.height:
+                    ray.append((nr, nc))
+            rays.append(ray)
+
+        if diagonals:
+            for dr, dc in Ordinals:
+                ray = []
+                for i in range(1, scaling + 1):
+                    nr, nc = r + dr * i, c + dc * i
+                    if 0 <= nr < self.width and 0 <= nc < self.height:
+                        ray.append((nr, nc))
+                rays.append(ray)
+
+        return rays
+
+    def get_neighbour_ray_values(
+        self, r: int, c: int, scaling: int, diagonals: bool = False
+    ) -> list[T]:
+        """Get the values of the neighbours of a cell at a given row, col in the grid.  Always from clockwise starting from the top.
+
+        Args:
+            r (int): Row of the cell.
+            c (int): Column of the cell.
+            scaling (int): How far to go in each direction.
+            diagonals (bool, optional): If we should include diagonals of the cell. Defaults to False.
+
+        Returns:
+            list[T]: List of the values of the neighbours of the cell.
+        """
+        return [
+            [self.grid[nr][nc] for nr, nc in ray]
+            for ray in self.get_neighbour_rays(r, c, scaling, diagonals)
+        ]
+
+    def __getitem__(self, row: int) -> list[T]:
+        """Get the value at a given row in the grid.
+
+        Args:
+            row (int): Row of the grid.
+
+        Returns:
+            list[T]: List of values at the given row.
+        """
+        return self.grid[row]
+
+    def __setitem__(self, row: int, value: list[T]) -> None:
+        """Set the value at a given row in the grid.
+
+        Args:
+            row (int): Row of the grid.
+            value (list[T]): Value to set at the given row.
+        """
+        self.grid[row] = value
+
+    def __repr__(self):
+        """String representation of the grid.
+
+        Returns:
+            str: String representation of the grid.
+        """
+        return "\n".join(["".join([str(cell) for cell in row]) for row in self.grid])
