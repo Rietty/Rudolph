9577933766c65eacdb73554c08f7e4106c626b6b
Juliette H. Lavoie <35241556+Rietty@users.noreply.github.com>
2024-12-23 09:56:46 -0500

    Update graph library to remove networkx wrapper and only have helpful functions

--- src/library/graph.py
+++ src/library/graph.py
@@ -7,52 +7,27 @@ from loguru import logger as log
 from library.grid import Grid
 
 
-class Graph:
-    """Graph class for common graph operations. Uses NetworkX as the underlying graph library."""
+def graph_from_grid[
+    T
+](
+    grid: Grid,
+    directed: bool = False,
+    allowed: list[T] | None = None,
+    blocked: list[T] | None = None,
+    function: Callable | None = None,
+) -> None:
+    graph = nx.DiGraph() if directed else nx.Graph()
 
-    def __init__(self, directed=False):
-        """Initialize the Graph object with a directed or undirected graph.
+    for r in range(grid.width):
+        for c in range(grid.height):
+            if blocked and grid[r][c] in blocked:
+                continue
+            for dr, dc in grid.get_neighbours(r, c):
+                if allowed is None or grid[dr][dc] in allowed:
+                    if function:
+                        if function(grid[r][c], grid[dr][dc]):
+                            graph.add_edge((r, c), (dr, dc))
+                    else:
+                        graph.add_edge((r, c), (dr, dc))
 
-        Args:
-            directed (bool, optional): Indicates if edges are directed or not. Defaults to False.
-        """
-        self._graph = nx.DiGraph() if directed else nx.Graph()
-
-    def __getattr__(self, name):
-        if hasattr(self._graph, name):
-            return getattr(self._graph, name)
-        raise AttributeError(
-            f"'{type(self).__name__}' object has no attribute '{name}'"
-        )
-
-    def get(self):
-        """Return the underlying NetworkX graph object.
-
-        Returns:
-            nx.Graph or nx.DiGraph: The underlying NetworkX graph object.
-        """
-        return self._graph
-
-    def populate_from_grid[
-        T
-    ](
-        self,
-        grid: Grid,
-        allowed: list[T] | None = None,
-        blocked: list[T] | None = None,
-        function: Callable | None = None,
-    ) -> None:
-        for r in range(grid.width):
-            for c in range(grid.height):
-                if blocked and grid[r][c] in blocked:
-                    continue
-                for dr, dc in grid.get_neighbours(r, c):
-                    if allowed is None or grid[dr][dc] in allowed:
-                        if function:
-                            if function(grid[r][c], grid[dr][dc]):
-                                self.add_edge((r, c), (dr, dc))
-                        else:
-                            self.add_edge((r, c), (dr, dc))
-
-    def __repr__(self):
-        return f"{self._graph}"
+    return graph
