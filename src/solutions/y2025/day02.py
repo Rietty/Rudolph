c1200c7a7df46b44d02b04f9ab1c32d6a17e9aa1
Rietty <rie.juliette@gmail.com>
2025-12-02 00:05:23 -0500

    Finished 2025, Day 2.

---
+++ src/solutions/y2025/day02.py
@@ -0,0 +1,50 @@
+from utils.decorators import benchmark
+
+
+def is_repeat_twice(n: int) -> bool:
+    s = str(n)
+    if len(s) & 1:
+        return False
+    h = len(s) // 2
+    return s[:h] == s[h:]
+
+
+def is_repeated(n: int) -> bool:
+    s = str(n)
+    L = len(s)
+    for d in range(1, L // 2 + 1):
+        if L % d:
+            continue
+        if s == s[:d] * (L // d):
+            return True
+    return False
+
+
+@benchmark
+def part_a(data: list[tuple[int, int]]) -> int:
+    sum = 0
+    for low, high in data:
+        for i in range(low, high + 1):
+            if is_repeat_twice(i):
+                sum += i
+    return sum
+
+
+@benchmark
+def part_b(data: list[tuple[int, int]]) -> int:
+    sum = 0
+    for low, high in data:
+        for i in range(low, high + 1):
+            if is_repeated(i):
+                sum += i
+    return sum
+
+
+@benchmark
+def parse(data: str) -> list[tuple[int, int]]:
+    return [tuple(map(int, seg.split("-"))) for seg in data.split(",")]
+
+
+test_data_a = """11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"""
+
+test_data_b = test_data_a
