331e80998821f3b2aef5e21b2132a242b78d6a2c
Rietty <rie.juliette@gmail.com>
2025-12-03 12:36:55 -0500

    Make code shorter

--- src/solutions/y2025/day03.py
+++ src/solutions/y2025/day03.py
@@ -2,47 +2,22 @@ from utils.decorators import benchmark
 
 
 def max_joltage(input: int, n: int) -> int:
-    # Length of input number
-    if input == 0:
-        L = 1
-    else:
-        t = input
-        L = 0
-        while t > 0:
-            t //= 10
-            L += 1
-
-    # If number of required digits is larger than number, we just return number.
+    s = str(input)
+    L = len(s)
     if n >= L:
         return input
-
-    # Figure out how many digits to remove
     remove = L - n
-
-    # Yay for monotonic stacks!
-    stack = []
-
-    # Go thru digits and basically we add whatever digit is bigger at the end.
-    for i in range(L - 1, -1, -1):
-        current_digit = input // (10**i) % 10
-
-        while remove > 0 and stack and stack[-1] < current_digit:
-            stack.pop()
+    st = []
+    ap = st.append
+    pop = st.pop
+    for d in s:
+        while remove and st and st[-1] < d:
+            pop()
             remove -= 1
-
-        stack.append(current_digit)
-
-    # Remove however many digits left in the stack
-    final_digits = stack[:n]
-
-    # Reconstruct number using power of 10
-    result = 0
-    for i in range(len(final_digits)):
-        digit = final_digits[i]
-        power = n - 1 - i
-        result += digit * (10**power)
-
-    return result
+        ap(d)
+    if remove:
+        st = st[:-remove]
+    return int("".join(st))
 
 
 @benchmark
