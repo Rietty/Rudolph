7cd4becbad2c755d1887aef852436e0c6d363782
Rietty <rie.juliette@gmail.com>
2025-12-05 11:18:51 -0500

    Swap to intervaltree

--- src/solutions/y2025/day05.py
+++ src/solutions/y2025/day05.py
@@ -1,51 +1,40 @@
 import re
 
+from intervaltree import IntervalTree
+
 from utils.decorators import benchmark
 
-type Ranges = list[tuple[int, int]]
+type Ranges = IntervalTree
 type Ingredients = list[int]
 
 
 @benchmark
 def part_a(data: tuple[Ranges, Ingredients]) -> int:
     ranges, ingredients = data
-    fresh = 0
-    for x in ingredients:
-        for a, b in ranges:
-            if a <= x <= b:
-                fresh += 1
-                break
-    return fresh
+    return sum(1 for i in ingredients if ranges[i])
 
 
 @benchmark
 def part_b(data: tuple[Ranges, Ingredients]) -> int:
     ranges, _ = data
-    ranges = sorted(ranges, key=lambda x: x[0])
-    merged: Ranges = []
-
-    # Basically going to merge overlapping ranges together as much as we can.
-    i, j = ranges[0]
-
-    for a, b in ranges[1:]:
-        if a <= j + 1:
-            j = max(j, b)
-        else:
-            merged.append((i, j))
-            i, j = a, b
-
-    merged.append((i, j))
-
-    # Whatever is left over, we iterate and sum up all the differences between end and start of range.
-    return sum(end - start + 1 for start, end in merged)
+    ranges.merge_overlaps(strict=False)
+    return sum(iv.end - iv.begin for iv in ranges)
 
 
 @benchmark
-def parse(data: str) -> tuple[Ranges, Ingredients]:
-    a, b = re.split(r"\n\s*\n", data.strip())  # God bless regex.
-    ranges = [tuple(map(int, x.split("-"))) for x in a.splitlines() if x.strip()]
-    available = [int(x) for x in b.splitlines() if x.strip()]
-    return ranges, available
+def parse(data: str) -> tuple[IntervalTree, Ingredients]:
+    r, i = re.split(r"\n\s*\n", data.strip())
+
+    ranges = IntervalTree()
+    for line in r.splitlines():
+        if not line.strip():
+            continue
+        begin, end = map(int, line.split("-"))
+        ranges[begin : end + 1] = (begin, end + 1)
+
+    ingredients = [int(v) for v in i.splitlines() if v.strip()]
+
+    return ranges, ingredients
 
 
 test_data_a = """
