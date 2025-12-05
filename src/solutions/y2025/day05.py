bda63ecf2ce89e9932bda37339e9d2fbea3fa092
Rietty <rie.juliette@gmail.com>
2025-12-05 00:14:40 -0500

    Finished day 5

--- src/solutions/y2025/day05.py
+++ src/solutions/y2025/day05.py
@@ -1,22 +1,64 @@
+import re
+
 from utils.decorators import benchmark
 
 
 @benchmark
-def part_a(data: list[str]) -> int:
-    return 0
+def part_a(data: tuple[list[tuple[int, int]], list[int]]) -> int:
+    ranges, ingredients = data
+    c = 0
+    for x in ingredients:
+        for a, b in ranges:
+            if a <= x <= b:
+                c += 1
+                break
+    return c
 
 
 @benchmark
-def part_b(data: list[str]) -> int:
-    return 0
+def part_b(data: tuple[list[tuple[int, int]], list[int]]) -> int:
+    ranges, _ = data
+    ranges = sorted(ranges, key=lambda x: x[0])
+    merged: list[tuple[int, int]] = []
+
+    i, j = ranges[0]
+
+    for a, b in ranges[1:]:
+        if a <= j + 1:
+            j = max(j, b)
+        else:
+            merged.append((i, j))
+            i, j = a, b
+
+    merged.append((i, j))
+
+    return sum(end - start + 1 for start, end in merged)
 
 
 @benchmark
-def parse(data: str) -> list[str]:
-    return data.splitlines()
+def parse(data: str) -> tuple[list[tuple[int, int]], list[int]]:
+    a, b = re.split(r"\n\s*\n", data.strip())
+
+    def pr(s):
+        return tuple(map(int, s.split("-")))
+
+    ranges = [pr(x) for x in a.splitlines() if x.strip()]
+    available = [int(x) for x in b.splitlines() if x.strip()]
+    return ranges, available
 
 
 test_data_a = """
+3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32
 """
 
 test_data_b = test_data_a
