d32403a3d5e44b1fc98f5ca6849c132ab0807810
Rietty <rie.juliette@gmail.com>
2025-12-07 00:38:56 -0500

    finished day 7

--- src/solutions/y2025/day07.py
+++ src/solutions/y2025/day07.py
@@ -4,41 +4,24 @@ from utils.decorators import benchmark
 # Instead of a graph easier to model this problem row by row instead and just.. count?
 # Why the fuck didn't I think of this ages ago???
 def emitter(data: list[str]) -> tuple[int, int]:
-    W = len(data[0])
-    start = data[0].index("S")
-
-    beams = {start}
-    counts = {start: 1}
+    counts = {data[0].index("S"): 1}
     splits = 0
 
-    for row in range(1, len(data)):
-        line = data[row]
-
-        new_beams = set()
-        new_counts = {}
-
-        for c in beams:
-            if 0 <= c < W and line[c] == "^":
-                splits += 1
-                if c - 1 >= 0:
-                    new_beams.add(c - 1)
-                if c + 1 < W:
-                    new_beams.add(c + 1)
-            else:
-                if 0 <= c < W:
-                    new_beams.add(c)
-
+    for line in data[1:]:
+        nb, nc = set(), {}
         for c, n in counts.items():
-            if 0 <= c < W and line[c] == "^":
-                for nc in (c - 1, c + 1):
-                    if 0 <= nc < W:
-                        new_counts[nc] = new_counts.get(nc, 0) + n
+            hit = 0 <= c < len(data[0]) and line[c] == "^"
+            if hit:
+                splits += 1
+                for nc2 in (c - 1, c + 1):
+                    if 0 <= nc2 < len(data[0]):
+                        nb.add(nc2)
+                        nc[nc2] = nc.get(nc2, 0) + n
             else:
-                if 0 <= c < W:
-                    new_counts[c] = new_counts.get(c, 0) + n
-
-        beams = new_beams
-        counts = new_counts
+                if 0 <= c < len(data[0]):
+                    nb.add(c)
+                    nc[c] = nc.get(c, 0) + n
+        counts = nc
 
     return splits, sum(counts.values())
 
