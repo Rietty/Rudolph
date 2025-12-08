4555b94a7a4f900f95248dee0a2a722dea504627
Rietty <rie.juliette@gmail.com>
2025-12-08 00:49:09 -0500

    Done day 8

--- src/library/geometry.py
+++ src/library/geometry.py
@@ -9,6 +9,9 @@ class Point:
     x: int
     y: int
 
+    def __hash__(self):
+        return (self.x << 32) ^ self.y
+
     def __add__(self, other):
         return Point(self.x + other.x, self.y + other.y)
 
@@ -52,6 +55,58 @@ class Point:
         return f"Point({self.x}, {self.y})"
 
 
+@dataclass
+class Point3D:
+    x: int
+    y: int
+    z: int
+
+    def __hash__(self):
+        return (self.x << 40) ^ (self.y << 20) ^ self.z
+
+    def __add__(self, other):
+        return Point3D(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __sub__(self, other):
+        return Point3D(self.x - other.x, self.y - other.y, self.z - other.z)
+
+    def __mul__(self, other):
+        return Point3D(self.x * other.x, self.y * other.y, self.z * other.z)
+
+    def __truediv__(self, other):
+        return Point3D(self.x / other.x, self.y / other.y, self.z / other.z)
+
+    def __floordiv__(self, other):
+        return Point3D(self.x // other.x, self.y // other.y, self.z // other.z)
+
+    def __mod__(self, other):
+        return Point3D(self.x % other.x, self.y % other.y, self.z % other.z)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y and self.z == other.z
+
+    def __ne__(self, other):
+        return self.x != other.x or self.y != other.y or self.z != other.z
+
+    def __lt__(self, other):
+        return self.x < other.x and self.y < other.y and self.z < other.z
+
+    def __le__(self, other):
+        return self.x <= other.x and self.y <= other.y and self.z <= other.z
+
+    def __gt__(self, other):
+        return self.x > other.x and self.y > other.y and self.z > other.z
+
+    def __ge__(self, other):
+        return self.x >= other.x and self.y >= other.y and self.z >= other.z
+
+    def __str__(self):
+        return f"({self.x}, {self.y}, {self.z})"
+
+    def __repr__(self):
+        return f"Point3D({self.x}, {self.y}, {self.z})"
+
+
 def manhattan_distance(a: Point, b: Point) -> int:
     return abs(a.x - b.x) + abs(a.y - b.y)
 
@@ -62,3 +117,15 @@ def euclidean_distance(a: Point, b: Point) -> float:
 
 def chebyshev_distance(a: Point, b: Point) -> int:
     return max(abs(a.x - b.x), abs(a.y - b.y))
+
+
+def manhattan_distance3D(a: Point3D, b: Point3D) -> int:
+    return abs(a.x - b.x) + abs(a.y - b.y) + abs(a.z - b.z)
+
+
+def euclidean_distance3D(a: Point3D, b: Point3D) -> float:
+    return ((a.x - b.x) ** 2 + (a.y - b.y) ** 2 + (a.z - b.z) ** 2) ** 0.5
+
+
+def chebyshev_distance3D(a: Point3D, b: Point3D) -> int:
+    return max(abs(a.x - b.x), abs(a.y - b.y), abs(a.z - b.z))
